8f61494e7b68e76ba7b8aa7bf6d9a15197c7d863
Teste <narjaracatherine@gmail.com>
2024-04-14 16:34:46 -0300

    Leia 3 números e escreva em ordem crescente

---
+++ Ordem Crescente.py
@@ -0,0 +1,23 @@
+try:
+    a = int(input("A = "))
+    b = int(input("B = "))
+    c = int(input("C = "))
+except:
+    print("Erro")
+else:
+    if a < b and b < c:
+        print(a, b, c)
+    elif a < c and c < b:
+        print(a, c, b)
+    elif b < a and a < c:
+        print(b, a, c)
+    elif b < c and c < a:
+        print(b, c, a)
+    elif c < a and a < b:
+        print(c, a, b)
+    elif c < b and b < a:
+        print(c, b, a)
+    else:
+        print("Invalido")
+finally:
+    print("Volte sempre")
